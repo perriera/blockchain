cace5bd6ca006953cf5d5945058e35e26d6d7988
Perry Anderson <perry.anderson@gmail.com>
2021-07-07 11:18:58 -0400

    Added first FakeIt test case

--- test/test_FakeIt.cpp
+++ test/test_FakeIt.cpp
@@ -10,11 +10,34 @@
 #include "../extra/include/string_support.hpp"
 #include "../include/HelloWorld.hpp"
 #include "catch.hpp"
+#include "fakeit.hpp"
+
+struct SomeInterface {
+  virtual int foo(int) = 0;
+  virtual int bar(int, int) = 0;
+};
 
 using namespace std;
+using namespace fakeit;
 
 SCENARIO("Verify FakeIt", "[FakeIt]") {
 
-  HelloWorld helloWorld;
-  REQUIRE(helloWorld.say_hello() == "Hello, world");
+  Mock<SomeInterface> mock;
+  // Stub a method to return a value once
+  When(Method(mock, foo)).Return(1);
+
+  // Stub multiple return values (The next two lines do exactly the same)
+  When(Method(mock, foo)).Return(1, 2, 3);
+  When(Method(mock, foo)).Return(1).Return(2).Return(3);
+
+  // Return the same value many times (56 in this example)
+  When(Method(mock, foo)).Return(56_Times(1));
+
+  // Return many values many times (First 100 calls will return 1, next 200
+  // calls will return 2)
+  When(Method(mock, foo)).Return(100_Times(1), 200_Times(2));
+
+  // Always return a value (The next two lines do exactly the same)
+  When(Method(mock, foo)).AlwaysReturn(1);
+  Method(mock, foo) = 1;
 }
